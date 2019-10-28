99ca7768ca7f006c16bcbaeb76c5472b7e2708f9
fei.song <songfei@lob-inc.com>
2019-10-28 16:00:07 +0900

    26.remove-duplicates-from-sorted-array.go

---
+++ 26.remove-duplicates-from-sorted-array.go
@@ -0,0 +1,25 @@
+package main
+
+/*
+ * @lc app=leetcode id=26 lang=golang
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+
+// @lc code=start
+func removeDuplicates(nums []int) int {
+	if len(nums) < 2 {
+		return len(nums)
+	}
+	n := 1
+	for i, j := 0, 0; i < len(nums); i++ {
+		if nums[j] != nums[i] {
+			j++
+			nums[j] = nums[i]
+			n++
+		}
+	}
+	return n
+}
+
+// @lc code=end
---
+++ 26.remove-duplicates-from-sorted-array_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_removeDuplicates(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"0", args{[]int{}}, 0},
+		{"1", args{[]int{1}}, 1},
+		{"2", args{[]int{1, 1}}, 1},
+		{"3", args{[]int{1, 1, 2}}, 2},
+		{"4", args{[]int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}}, 5},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeDuplicates(tt.args.nums); got != tt.want {
+				t.Errorf("removeDuplicates() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
