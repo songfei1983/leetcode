d05e9f84da0969b3d77d27ebe73c3e20412d7a75
fei.song <songfei@lob-inc.com>
2019-10-30 13:03:43 +0900

    501.find-mode-in-binary-search-tree.go

---
+++ 501.find-mode-in-binary-search-tree_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_findMode(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"1", args{&TreeNode{1, nil, &TreeNode{2, &TreeNode{2, nil, nil}, nil}}}, []int{2}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findMode(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findMode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
