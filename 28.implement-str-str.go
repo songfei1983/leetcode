dfbce3a358b5ce9ffe30d9699158d3d31cf3c9d9
songfei1983 <runtime1983@gmail.com>
2019-11-05 22:10:30 +0900

    28.implement-str-str.go

---
+++ 28.implement-str-str.go
@@ -0,0 +1,24 @@
+package main
+
+/*
+ * @lc app=leetcode id=28 lang=golang
+ *
+ * [28] Implement strStr()
+ */
+
+// @lc code=start
+func strStr(haystack string, needle string) int {
+	if haystack == needle || needle == "" {
+		return 0
+	}
+	bs := []byte(haystack)
+	x, y := len(bs), len(needle)
+	for i := 0; i <= x-y; i++ {
+		if string(bs[i:i+y]) == needle {
+			return i
+		}
+	}
+	return -1
+}
+
+// @lc code=end
