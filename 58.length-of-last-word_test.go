40718a4db62589a7859f674a79a972d692c74329
songfei1983 <runtime1983@gmail.com>
2019-11-12 17:14:00 +0900

    58.length-of-last-word.go

---
+++ 58.length-of-last-word_test.go
@@ -0,0 +1,28 @@
+package main
+
+import "testing"
+
+func Test_lengthOfLastWord(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"1", args{"hello world"}, 5},
+		{"2", args{""}, 0},
+		{"3", args{"song fei"}, 3},
+		{"4", args{"song fei "}, 3},
+		{"5", args{"song fei  "}, 3},
+		{"6", args{"    "}, 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lengthOfLastWord(tt.args.s); got != tt.want {
+				t.Errorf("lengthOfLastWord() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
